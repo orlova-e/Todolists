7a2b65ccbc1d914e83f1a7ef596346d11dd61fe5
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 17:46:48 +0300

    add AddCommonInfrastructure() extension method

---
+++ src/Todolists/Todolists.Infrastructure.DataAccess/Configuration/CommonInfrastructureExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Todolists.Infrastructure.DataAccess.Implementation;
+
+namespace Todolists.Infrastructure.DataAccess.Configuration;
+
+public static class CommonInfrastructureExtensions
+{
+    public static IServiceCollection AddCommonInfrastructure(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IRepository, Repository>();
+
+        return services;
+    }
+}
