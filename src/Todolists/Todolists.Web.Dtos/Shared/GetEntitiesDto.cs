67690abb381454de89815447b124f02ca46c23a0
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 18:42:37 +0300

    add shared dto

---
+++ src/Todolists/Todolists.Web.Dtos/Shared/GetEntitiesDto.cs
@@ -0,0 +1,13 @@
+﻿using Todolists.Domain.Core.Conditions;
+
+namespace Todolists.Web.Dtos.Shared
+{
+    public class GetEntitiesDto
+    {
+        public int Page { get; set; } = 1;
+        public int ItemsNumber { get; set; } = 20;
+        public SortDir SortDir { get; set; } = SortDir.Desc;
+        public string OrderBy { get; set; }
+        public string Filters { get; set; }
+    }
+}
---
+++ src/Todolists/Todolists.Web.Dtos/Shared/ListDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Todolists.Domain.Core.Conditions;
+
+namespace Todolists.Web.Dtos.Shared
+{
+    public class ListDto<T>
+    {
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public SortDir ListSorting { get; set; }
+        public IEnumerable<T> Entities { get; set; }
+        public string Filters { get; set; }
+    }
+}
