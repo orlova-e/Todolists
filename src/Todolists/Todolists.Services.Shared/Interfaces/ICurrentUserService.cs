20d695300310c868551994bbe8be59ab0fcf70cf
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 18:03:32 +0300

    add shared services interfaces

---
+++ src/Todolists/Todolists.Services.Shared/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,10 @@
+﻿using Todolists.Domain.Core.Entities;
+
+namespace Todolists.Services.Shared.Interfaces;
+
+public interface ICurrentUserService
+{
+    Guid UserId { get; }
+    Task<User> GetAsync(CancellationToken cancellationToken);
+    User Get();
+}
---
+++ src/Todolists/Todolists.Services.Shared/Interfaces/IDateTimeService.cs
@@ -0,0 +1,11 @@
+﻿using Todolists.Domain.Core.Interfaces;
+
+namespace Todolists.Services.Shared.Interfaces;
+
+public interface IDateTimeService
+{
+    DateTime UtcNow { get; }
+    void Created<T>(T entity) where T : class, IHistorical;
+    void Updated<T>(T entity) where T : class, IHistorical;
+    void Deleted<T>(T entity) where T : class, IHistorical;
+}
