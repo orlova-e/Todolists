50d6a1c8fe725c3a24ccfa0eff8669c3df311bdb
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 18:51:58 +0300

    add validator and validation messages

---
+++ src/Todolists/Todolists.Web.Identity/Validation/LoginDtoValidator.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation;
+using Todolists.Domain.Core.Entities;
+using Todolists.Domain.Services.Specs.Domain;
+using Todolists.Infrastructure.DataAccess;
+using Todolists.Services.Shared.Interfaces;
+using Todolists.Web.Identity.Dto;
+
+namespace Todolists.Web.Identity.Validation
+{
+    internal class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        private readonly IRepository _repository;
+        private readonly IEncryptionService _encryptionService;
+
+        public LoginDtoValidator(
+            IRepository repository,
+            IEncryptionService encryptionService)
+        {
+            _repository = repository;
+            _encryptionService = encryptionService;
+
+            RuleFor(x => x.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(ValidationMessages.EmailRequired)
+                .MustAsync(DoesAccountExists)
+                .WithMessage(ValidationMessages.AccountDoesNotExist);
+
+            RuleFor(x => x.Password)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(ValidationMessages.PasswordRequired)
+                .MinimumLength(8)
+                .WithMessage(ValidationMessages.PasswordMinLength)
+                .MustAsync(IsPasswordValid)
+                .WithMessage(ValidationMessages.PasswordInvalid);
+        }
+
+        private async Task<bool> DoesAccountExists(
+            LoginDto dto,
+            string email,
+            CancellationToken cancellationToken)
+        {
+            var count = await _repository.CountAsync<Account, Guid>(
+                Accounts.ByEmailOrUsername(email) &
+                Common.NotDeleted<Account>(), cancellationToken);
+            
+            return count is 1;
+        }
+
+        private async Task<bool> IsPasswordValid(
+            LoginDto dto, 
+            string password,
+            CancellationToken cancellationToken)
+        {
+            var account = await _repository.GetAsync<Account, Guid>(
+                Accounts.ByEmail(dto.Name) &
+                Common.NotDeleted<Account>(), cancellationToken);
+            
+            return account is not null &&
+                   _encryptionService.Validate(account.Hash, dto.Password);
+        }
+    }
+}
---
+++ src/Todolists/Todolists.Web.Identity/Validation/ValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace Todolists.Web.Identity.Validation
+{
+    internal static class ValidationMessages
+    {
+        public const string EmailRequired = "Email is required";
+        public const string AccountDoesNotExist = "The account is missing in the system";
+        
+        public const string PasswordRequired = "The password is required";
+        public const string PasswordMinLength = "The password must contain at least 8 characters";
+        public const string PasswordInvalid = "The password is invalid";
+    }
+}
