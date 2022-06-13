50387164e4d67b812a924887ed936eb7ad3c4fb0
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 18:04:02 +0300

    add AddSharedServices() extension method

---
+++ src/Todolists/Todolists.Services.Shared/Configuration/SharedServicesExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Todolists.Services.Shared.Implementation;
+using Todolists.Services.Shared.Interfaces;
+
+namespace Todolists.Services.Shared.Configuration;
+
+public static class SharedServicesExtensions
+{
+    public static IServiceCollection AddSharedServices(
+        this IServiceCollection services,
+        Assembly mappingAssembly = null)
+    {
+        services
+            .AddSingleton<IEncryptionService, EncryptionService>()
+            .AddSingleton<IDateTimeService, DateTimeService>();
+
+        if (mappingAssembly is not null)
+        {
+            services
+                .AddTransient<ITranslator, Translator>()
+                .AddAutoMapper(mappingAssembly);
+        }
+
+        return services;
+    }
+}
