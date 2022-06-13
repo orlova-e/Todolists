90c6fae17c96e4654a7ee8dab292fe14aed7b399
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 17:56:23 +0300

    add migrations

---
+++ src/Todolists/Todolists.Infrastructure.Versions/Migrations/Migration1.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace Todolists.Infrastructure.Versions.Migrations;
+
+[Migration(1, "add users table")]
+public class Migration1 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Table("users")
+            .WithColumn("id").AsGuid().PrimaryKey()
+            .WithColumn("isdeleted").AsBoolean().NotNullable()
+            .WithColumn("name").AsString().NotNullable()
+            .WithColumn("created").AsDateTime().NotNullable()
+            .WithColumn("updated").AsDateTime().Nullable()
+            .WithColumn("deleted").AsDateTime().Nullable();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Table("users");
+    }
+}
---
+++ src/Todolists/Todolists.Infrastructure.Versions/Migrations/Migration4.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace Todolists.Infrastructure.Versions.Migrations;
+
+[Migration(4, "add checklists table")]
+public class Migration4 : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Table("checklists")
+            .WithColumn("id").AsGuid().PrimaryKey();
+        
+        Create
+            .ForeignKey("FK_checklists_notebases_id")
+            .FromTable("checklists").ForeignColumn("id")
+            .ToTable("notebases").PrimaryColumn("id");
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Table("checklists");
+    }
+}
