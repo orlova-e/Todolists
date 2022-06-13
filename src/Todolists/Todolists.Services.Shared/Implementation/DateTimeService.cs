5050151b651a614f226a459a95508584d1ecf749
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 18:03:47 +0300

    add shared services implementation

---
+++ src/Todolists/Todolists.Services.Shared/Implementation/DateTimeService.cs
@@ -0,0 +1,18 @@
+﻿using Todolists.Domain.Core.Interfaces;
+using Todolists.Services.Shared.Interfaces;
+
+namespace Todolists.Services.Shared.Implementation;
+
+public class DateTimeService : IDateTimeService
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+    
+    public void Created<T>(T entity) where T : class, IHistorical
+        => entity.Created = this.UtcNow;
+
+    public void Updated<T>(T entity) where T : class, IHistorical
+        => entity.Updated = this.UtcNow;
+
+    public void Deleted<T>(T entity) where T : class, IHistorical
+        => entity.Deleted = this.UtcNow;
+}
---
+++ src/Todolists/Todolists.Services.Shared/Implementation/EncryptionService.cs
@@ -0,0 +1,12 @@
+﻿using Todolists.Services.Shared.Interfaces;
+
+namespace Todolists.Services.Shared.Implementation;
+
+internal class EncryptionService : IEncryptionService
+{
+    public string ComputeHash(string value)
+        => BCrypt.Net.BCrypt.HashPassword(value);
+
+    public bool Validate(string hash, string password)
+        => BCrypt.Net.BCrypt.Verify(password, hash);
+}
