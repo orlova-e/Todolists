f7d1942fe58327bba4ca55758d9b845823ea1e8a
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 19:54:31 +0300

    add Program.cs for API project

--- src/Todolists/Todolists.Web.API/Program.cs
+++ src/Todolists/Todolists.Web.API/Program.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Todolists.Infrastructure.Base.Configuration;
+using Todolists.Infrastructure.DataAccess.Configuration;
+using Todolists.Services.Shared.Configuration;
+using Todolists.Web.API.Configuration;
+using Todolists.Web.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services
+    .AddBaseInfrastructure(builder.Configuration)
+    .AddCommonInfrastructure()
+    .AddSharedServices(typeof(Program).Assembly)
+    .AddWebServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app
+    .UseForwardedHeaders(new ForwardedHeadersOptions 
+        {ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto})
+    .UseRouting()
+    .UseHttpsRedirection()
+    .UseLoggingMiddleware()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
 
 app.Run();
