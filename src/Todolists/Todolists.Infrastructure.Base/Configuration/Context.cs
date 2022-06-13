b6a9696dbed89c15c07b759f57b0657c19e75596
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 17:42:34 +0300

    add db context inheritor

---
+++ src/Todolists/Todolists.Infrastructure.Base/Configuration/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todolists.Domain.Core.Entities;
+using Todolists.Infrastructure.Base.Mappings;
+
+namespace Todolists.Infrastructure.Base.Configuration;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+    }
+}
