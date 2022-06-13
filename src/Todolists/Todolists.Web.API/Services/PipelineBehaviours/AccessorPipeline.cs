05699f3f878486e6254fcc1e05b45562321fdd76
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-13 19:51:53 +0300

    add AccessorPipeline.cs and ValidationPipeline.cs

---
+++ src/Todolists/Todolists.Web.API/Services/PipelineBehaviours/AccessorPipeline.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Todolists.Web.API.Services.Accessors;
+using Todolists.Web.API.Services.Commands;
+
+namespace Todolists.Web.API.Services.PipelineBehaviours;
+
+public sealed class AccessorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<AccessorBehavior<TRequest, TResponse>> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public AccessorBehavior(
+        ILogger<AccessorBehavior<TRequest, TResponse>> logger,
+        IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
+    }
+    
+    public async Task<TResponse> Handle(
+        TRequest request,
+        CancellationToken cancellationToken,
+        RequestHandlerDelegate<TResponse> next)
+    {
+        var validator = _serviceProvider.GetService<AccessValidator<TRequest>>();
+        if (validator == null)
+            return await next();
+
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+            return await next();
+
+        return ActivatorUtilities.CreateInstance<TResponse>(
+            _serviceProvider,
+            OperationStatus.Forbidden);
+    }
+}
---
+++ src/Todolists/Todolists.Web.API/Services/PipelineBehaviours/ValidationPipeline.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Todolists.Web.API.Services.Validation;
+
+namespace Todolists.Web.API.Services.PipelineBehaviours;
+
+public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<ValidationBehavior<TRequest, TResponse>> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public ValidationBehavior(
+        ILogger<ValidationBehavior<TRequest, TResponse>> logger,
+        IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
+    }
+    
+    public async Task<TResponse> Handle(
+        TRequest request,
+        CancellationToken cancellationToken,
+        RequestHandlerDelegate<TResponse> next)
+    {
+        var validator = _serviceProvider.GetService<DataValidator<TRequest>>();
+        if (validator == null)
+            return await next();
+
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+            return await next();
+
+        return ActivatorUtilities.CreateInstance<TResponse>(
+            _serviceProvider,
+            validationResult.Errors);
+    }
+}
