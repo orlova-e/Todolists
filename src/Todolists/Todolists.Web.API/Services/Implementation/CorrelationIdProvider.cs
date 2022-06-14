b713432beebeee6ff63fc344ebfc1dad4bf1fbab
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-14 11:06:43 +0300

    add ICorrelationIdProvider implementation

---
+++ src/Todolists/Todolists.Web.API/Services/Implementation/CorrelationIdProvider.cs
@@ -0,0 +1,19 @@
+﻿using Todolists.Services.Messaging.Interfaces;
+
+namespace Todolists.Web.API.Services.Implementation;
+
+public class CorrelationIdProvider : ICorrelationIdProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CorrelationIdProvider(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
+    public Guid GetCorrelationId()
+    {
+        var correlationIdString = _httpContextAccessor.HttpContext.Session.GetString("CorrelationId");
+        return Guid.Parse(correlationIdString);
+    }
+}
