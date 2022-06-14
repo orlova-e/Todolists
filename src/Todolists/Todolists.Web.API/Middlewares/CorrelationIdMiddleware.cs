c6cf62e4ba8f7909742a038f792f934c578cbb39
Elizaveta Orlova <orlovaev11@gmail.com>
2022-06-14 11:07:07 +0300

    add CorrelationIdMiddleware.cs

---
+++ src/Todolists/Todolists.Web.API/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Todolists.Web.API.Middlewares;
+
+public class CorrelationIdMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CorrelationIdMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    
+    public async Task Invoke(HttpContext httpContext)
+    {
+        httpContext.Session.SetString("CorrelationId", Guid.NewGuid().ToString());
+        await _next(httpContext);
+    }
+}
+
+public static class CorrelationIdMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCorrelationIds(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<CorrelationIdMiddleware>();
+    }
+}
